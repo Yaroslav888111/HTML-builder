5449682c0b85cefebcac4830ac4c54a51c0f74f1
Yaroslav Turkov <turkov988@gmail.com>
2024-01-23 17:52:49 +0300

    :fix: add 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const readStream = fs.createReadStream("text.txt", { encoding: "utf-8"});
+const readStream = fs.createReadStream("01-read-file/text.txt", { encoding: "utf-8"});
 readStream.on("data", (data) => {
     process.stdout.write(data);
 });
