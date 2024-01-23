d2277b624c0da6476c121455ae8c398b811d6c47
Yaroslav Turkov <turkov988@gmail.com>
2024-01-23 21:25:19 +0300

    :fix: add text.txt file with input text

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,8 +1,19 @@
-const { stdin, stdout } = process;
-stdout.write("Write a text");
-stdin.on("data", (data) => {
-    stdout.write("You write, ");
-    stdout.write(data);
-    process.exit();
+const fs = require("fs");
+const path = require("path");
+const readline = require("node:readline");
+
+const { stdin: input, stdout: output } = require("node:process");
+const rl = readline.createInterface({input, output});
+var inputText = fs.createWriteStream(path.join(__dirname, "text.txt"));
+rl.on("line", (input) => {
+    if ( input === "exit" ) {
+        console.log("Goodbye!");
+        rl.close();
+    } else {
+        inputText.write(`${input}\n`);
+    }
 });
-process.on("exit", () => stdout.write("Bye-bye!"));
+rl.on("SIGINT", () => {
+    console.log("Goodbye man!");
+    rl.close;
+});
